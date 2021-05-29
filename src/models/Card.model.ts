d3afdac990251a56f7b0ff98ad3f01d08bc39892
Pholawat Tangsatit <pholawat.tangsatit@hotmail.com>
2021-05-29 18:16:10 +0700

    add Card compare functions

--- src/models/Card.model.ts
+++ src/models/Card.model.ts
@@ -30,7 +30,39 @@ export default class Card {
 		this.Rank = r;
 	}
 
-	public toString() {
+	public isLessThan(CardB: Card): boolean {
+		if (CardB.Rank === this.Rank) {
+			if (CardB.Suit > this.Suit) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			if (CardB.Rank > this.Rank) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public isMoreThan(CardB: Card) {
+		if (CardB.Rank === this.Rank) {
+			if (CardB.Suit < this.Suit) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			if (CardB.Rank < this.Rank) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	public toString(): string {
 		let beginWithVowels = new RegExp("^(a|e|i|o|u|A|E|I|O|U)");
 		if (beginWithVowels.test(Ranks[this.Rank])) {
 			return `This is an ${Ranks[this.Rank]} of ${Suits[this.Suit]}`;
